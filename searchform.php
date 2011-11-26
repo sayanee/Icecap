40aeabe1a59e9c3c6338e6906dd90d22d7b7b56d
Sayanee <sayanee@gmail.com>
2011-11-26 08:06:31 +0800

    placeholder for search input

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
       <label class="search" for="s">Search</label>
-      <input type="text" value="" name="s" id="s" />
+      <input type="text" placeholder="Find this blog!" value="" name="s" id="s" />
     </div>
 </form>
