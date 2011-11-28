938a6deb1fb143d913f13b0743a39d1373bc450a
Sayanee <sayanee@gmail.com>
2011-11-28 16:03:45 +0800

    added margin-bottom for paragraphs

--- footer.php
+++ footer.php
@@ -8,12 +8,19 @@
 
   </div><!--eo #wrapper-->
   
-  <script> // Change UA-XXXXX-X to be your site's ID
-     window._gaq = [['_setAccount','UAXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];
-     Modernizr.load({
-       load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
-     });
-   </script>
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-xxxxxxxx-x']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
 
    <!--[if lt IE 7 ]>
      <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
