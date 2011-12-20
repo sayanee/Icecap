38e3e61f3ef74a8e0b44e4135ccb461c6dcdd076
Gun.io Whitespace Robot <contact@gun.io>
2011-12-19 20:20:38 -0500

    Remove whitespace [Gun.io WhitespaceBot]

--- loop.php
+++ loop.php
@@ -5,9 +5,9 @@
 <?php endif; ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
-  
+
   <article class="clearfix">
-    
+
    <post>
       <h4><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
 
@@ -24,35 +24,35 @@
 
     	<?php comments_template( '', true ); ?>
   	</post>
-    	
-    	
+
+
     <side>
       <div id="circle"><div><?php comments_popup_link( __( '0', 'twentyten' ), __( '1', 'twentyten' ), __( '%', 'twentyten' ) ); ?></div></div>
-    	
+
     	<date><?php the_date(); ?></date>
-	    
+
 	    <list>
       	<?php if ( count( get_the_category() ) ) : ?>
       		<?php printf( __( '%2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
-      </list>		
+      </list>
       	<?php endif; ?>
-	
+
 	    <list>
       	<?php $tags_list = get_the_tag_list( '', ', ' ); if ( $tags_list ): ?>
       		<?php printf( __( '%2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
       	<?php endif; ?>
     	</list>
-    	
+
   	</side>
-  
+
 	</article>
 
 <?php endwhile; ?>
 
 <menu class="clearfix">
   <line></line>
-  
-  
+
+
   <?php if (  $wp_query->max_num_pages > 1 ) : ?>
     <div id="button" class="aligncenter">
     	<prev><?php next_posts_link( ''); ?></prev>
