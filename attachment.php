678d6dd6f7f427cddc6512acbd5b79644ea53fa5
Sayanee <sayanee@gmail.com>
2012-05-24 12:55:36 +0800

    gallery fixed

--- attachment.php
+++ attachment.php
@@ -1,14 +1,9 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-	<p><a href="<?php echo get_permalink( $post->post_parent ); ?>" title="<?php esc_attr( printf( __( 'Return to %s', 'icecap' ), get_the_title( $post->post_parent ) ) ); ?>" rel="gallery"><?php
-		/* translators: %s - title of parent post */
-		printf( __( '<span>&larr;</span> %s', 'icecap' ), get_the_title( $post->post_parent ) );
-		?></a></p>
-
-	<h2><?php the_title(); ?></h2>
-	<?php printf(__('By %2$s', 'icecap'), 'meta-prep meta-prep-author',
+	<article class="clearfix">
+		<side>
+			<?php printf(__('By %2$s', 'icecap'), 'meta-prep meta-prep-author',
 				sprintf( '<a class="url fn n" href="%1$s" title="%2$s">%3$s</a>',
 				get_author_posts_url( get_the_author_meta( 'ID' ) ),
 				sprintf( esc_attr__( 'View all posts by %s', 'icecap' ), get_the_author() ),
@@ -28,49 +23,65 @@
 				esc_attr( __('Link to full-size image', 'icecap') ),
 				$metadata['width'],
 				$metadata['height']));} ?>
-	<?php edit_post_link( __( 'Edit', 'icecap' ), '<p class="edit-post">', '</p>' ); ?>
-</div><!-- .entry-meta -->
 
-<?php if ( wp_attachment_is_image() ) :
-	$attachments = array_values( get_children( array( 'post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID' ) ) );
-	foreach ( $attachments as $k => $attachment ) {
-		if ( $attachment->ID == $post->ID )
-			break;
-	}
-	$k++;
-	// If there is more than 1 image attachment in a gallery
-	if ( count( $attachments ) > 1 ) {
-		if ( isset( $attachments[ $k ] ) )
-			// get the URL of the next image attachment
-			$next_attachment_url = get_attachment_link( $attachments[ $k ]->ID );
-		else
-			// or get the URL of the first image attachment
-			$next_attachment_url = get_attachment_link( $attachments[ 0 ]->ID );
-	} else {
-		// or, if there's only 1 image attachment, get the URL of the image
-		$next_attachment_url = wp_get_attachment_url();
-	}
-?>
-						<p><a href="<?php echo $next_attachment_url; ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php
-							$attachment_size = apply_filters( 'icecap_attachment_size', 900 );
-							echo wp_get_attachment_image( $post->ID, array( $attachment_size, 9999 ) ); // filterable image width with, essentially, no limit for image height.
-						?></a></p>
+		</side>
 
-							<?php previous_image_link( false ); ?>
-							<?php next_image_link( false ); ?>
-<?php else : ?>
-						<a href="<?php echo wp_get_attachment_url(); ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php echo basename( get_permalink() ); ?></a>
-<?php endif; ?>
-						<?php if ( !empty( $post->post_excerpt ) ) the_excerpt(); ?>
+		<post>
+			<h4><?php the_title(); ?></h4>
 
-<?php the_content( __( 'Continue reading &rarr;', 'icecap' ) ); ?>
-<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'icecap' ), 'after' => '' ) ); ?>
+			<?php if ( wp_attachment_is_image() ) : $attachments = array_values( get_children( array( 'post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID' ) ) );
+				foreach ( $attachments as $k => $attachment ) {
+					if ( $attachment->ID == $post->ID )
+					break;
+				}
+				$k++;
+				// If there is more than 1 image attachment in a gallery
+				if ( count( $attachments ) > 1 ) {
+					if ( isset( $attachments[ $k ] ) )
+						// get the URL of the next image attachment
+						$next_attachment_url = get_attachment_link( $attachments[ $k ]->ID );
+					else
+						// or get the URL of the first image attachment
+						$next_attachment_url = get_attachment_link( $attachments[ 0 ]->ID );
+				} else {
+					// or, if there's only 1 image attachment, get the URL of the image
+					$next_attachment_url = wp_get_attachment_url();
+				}
+			?>
+			<p>
+				<a href="<?php echo get_permalink( $post->post_parent ); ?>" title="<?php esc_attr( printf( __( 'Return to %s', 'icecap' ), get_the_title( $post->post_parent ) ) ); ?>" rel="gallery">
+					<?php printf( __( '<span>&larr;</span> %s', 'icecap' ), get_the_title( $post->post_parent ) );?>
+				</a>
+			</p>
 
-						<?php icecap_posted_in(); ?>
-						<?php edit_post_link( __( 'Edit', 'icecap' ), ' ', '' ); ?>
+			<p><a href="<?php echo $next_attachment_url; ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment">
+				<?php $attachment_size = apply_filters( 'icecap_attachment_size', 900 );
+					echo wp_get_attachment_image( $post->ID, array( $attachment_size, 9999 ) ); ?>
+				</a>
+			</p>
 
-<?php comments_template(); ?>
+			<?php previous_image_link( false ); ?>
+			<?php next_image_link( false ); ?>
+			<?php else : ?>
+				<a href="<?php echo wp_get_attachment_url(); ?>" title="<?php echo esc_attr( get_the_title() ); ?>" rel="attachment"><?php echo basename( get_permalink() ); ?></a>
+			<?php endif; ?>
+			
+			<?php if ( !empty( $post->post_excerpt ) ) the_excerpt(); ?>
+
+			<?php the_content( __( 'Continue reading &rarr;', 'icecap' ) ); ?>
+			
+			<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'icecap' ), 'after' => '' ) ); ?>
+			<?php icecap_posted_in(); ?>
+			<?php edit_post_link( __( 'Edit', 'icecap' ), '<p class="edit-post">', '</p>' ); ?>
+
+		</post>	
+	</article>
+
+	<nav class="clearfix"><line></line></nav>
+
+	<?php comments_template(); ?>
 
 <?php endwhile; ?>
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -18,19 +18,18 @@
       		<?php printf( __( '%2$s', 'icecap' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
       	<?php endif; ?>
     	</list>
-    	
     </side>
 
-		<post><div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		   <h4><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'icecap' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
-      
-      <p>
-        <?php the_content(); ?>
-        <?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'icecap' ), 'after' => '' ) ); ?>
-
-        <?php edit_post_link( __( 'Edit', 'icecap' ), '<p class="edit-post">', '</p>' ); ?>
-  		</p>
-		</div></post>
+		<post>
+      <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		    <h4><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'icecap' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
+        <p>
+          <?php the_content(); ?>
+          <?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'icecap' ), 'after' => '' ) ); ?>
+          <?php edit_post_link( __( 'Edit', 'icecap' ), '<p class="edit-post">', '</p>' ); ?>
+  		  </p>
+		  </div>
+    </post>
     
 	</article>
 	
